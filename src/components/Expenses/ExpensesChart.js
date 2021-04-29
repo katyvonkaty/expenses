6d5a7a989e450f6d72086a170cae6c95f33ed28a
katie <kj0203@gmail.com>
2021-04-29 16:56:38 -0400

    updated

---
+++ src/components/Expenses/ExpensesChart.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const ExpensesChart = (props) => {
+  const chartDataPoints = [
+    { label: "jan", value: 0 },
+    { label: "feb", value: 0 },
+    { label: "mar", value: 0 },
+    { label: "april", value: 0 },
+    { label: "may", value: 0 },
+    { label: "june", value: 0 },
+    { label: "july", value: 0 },
+    { label: "aug", value: 0 },
+    { label: "sept", value: 0 },
+    { label: "oct", value: 0 },
+    { label: "nov", value: 0 },
+  ];
+  return <Chart />;
+};
+
+export default ExpensesChart;
